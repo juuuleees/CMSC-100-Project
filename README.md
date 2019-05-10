c381dd6602b64fecc448835a52064ed151437e88
Julianne Ruz <jaruz@up.edu.ph>
2019-05-11 01:42:26 +0800

    Fixed bugs in database.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ so I didn't make any procedures for it yet.
 5-10-19, 10:37 PM - not sure how we're going to implement removing someone from the friends list so didn't include it for now.
 
 5-10-19, 11:48 PM - controllers and routers not yet complete but initial copies are present.
+
+5-11-19, 1:41 AM - bugs in database fixed, but have not populated data for other tables. Check file contents before sourcing.
--- server/database/posts_db_input.sql
+++ server/database/posts_db_input.sql
@@ -2,36 +2,42 @@ use social_net;
 
 insert into post (
 	post_date,
-	time_stamp,
+	post_time,
 	all_comments,
 	content,
+	user_id
 ) values (
 	"2019-01-01",
 	"08:00:00",
 	2,
 	"sample post",
+	4
 ),
 (
 	"2019-01-02",
 	"23:08:00",
 	1,
-	"another post"
+	"another post",
+	5
 ),
 (
 	"2019-03-22",
 	"02:39:07",
 	0,
-	"a fish fought off magellan"
+	"a fish fought off magellan",
+	5
 ),
 (
 	"2018-12-31",
 	"00:00:00",
 	1,
-	"i like cheese"
+	"i like cheese",
+	4
 ),
 (
 	"2017-07-22",
 	"09:14:53",
 	1,
-	"#hashtag"
+	"#hashtag",
+	5
 );
